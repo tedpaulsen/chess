264c213763abc460e66cfa2a71df6ba87441667d
Ted Paulsen <ted.m.paulsen@gmail.com>
2023-01-02 21:25:35 -0700

    Add queen move generation test

--- src/test/java/io/github/tedpaulsen/chess/lib/TestBase.java
+++ src/test/java/io/github/tedpaulsen/chess/lib/TestBase.java
@@ -1,36 +1,41 @@
 package io.github.tedpaulsen.chess.lib;
 
 import io.github.tedpaulsen.chess.lib.movegen.BishopMoveGen;
+import io.github.tedpaulsen.chess.lib.movegen.MoveGen;
 import io.github.tedpaulsen.chess.lib.movegen.PawnMoveGen;
+import io.github.tedpaulsen.chess.lib.movegen.QueenMoveGen;
 import io.github.tedpaulsen.chess.lib.movegen.RookMoveGen;
-import java.util.List;
-import java.util.function.BiFunction;
 
 public class TestBase {
 
     protected final BishopMoveGen bishopMoveGen = new BishopMoveGen();
     protected final RookMoveGen rookMoveGen = new RookMoveGen();
     protected final PawnMoveGen pawnMoveGen = new PawnMoveGen();
+    protected final QueenMoveGen queenMoveGen = new QueenMoveGen();
 
     public BitBoard generateBishopTargets(Side side, BoardRepresentation board) {
-        return generateTargets(side, board, bishopMoveGen::generateMoves);
+        return generateTargets(side, board, bishopMoveGen);
     }
 
     public BitBoard generateRookTargets(Side side, BoardRepresentation board) {
-        return generateTargets(side, board, rookMoveGen::generateMoves);
+        return generateTargets(side, board, rookMoveGen);
     }
 
     public BitBoard generatePawnTargets(Side side, BoardRepresentation board) {
-        return generateTargets(side, board, pawnMoveGen::generateMoves);
+        return generateTargets(side, board, pawnMoveGen);
     }
 
-    private BitBoard generateTargets(
-        Side side,
-        BoardRepresentation board,
-        BiFunction<Side, BoardRepresentation, List<Move>> moveGenerator
-    ) {
+    public BitBoard generateQueenTargets(Side side, BoardRepresentation board) {
+        return generateTargets(side, board, queenMoveGen);
+    }
+
+    private BitBoard generateTargets(Side side, BoardRepresentation board, MoveGen moveGenerator) {
         return new BitBoard(
-            moveGenerator.apply(side, board).stream().map(m -> m.getTo().getValue()).reduce(0L, (acc, el) -> acc | el)
+            moveGenerator
+                .generateMoves(side, board)
+                .stream()
+                .map(m -> m.getTo().getValue())
+                .reduce(0L, (acc, el) -> acc | el)
         );
     }
 }
---
+++ src/test/java/io/github/tedpaulsen/chess/lib/movegen/QueenMoveGenTest.java
@@ -0,0 +1,28 @@
+package io.github.tedpaulsen.chess.lib.movegen;
+
+import io.github.tedpaulsen.chess.lib.BoardRepresentation;
+import io.github.tedpaulsen.chess.lib.Side;
+import io.github.tedpaulsen.chess.lib.TestBase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class QueenMoveGenTest extends TestBase {
+
+    @Test
+    public void testQueenMoveGen() {
+        var b = BoardRepresentation.fromFen("r6k/p1p2Q1p/1bp4p/3pP3/3P2bq/2P1R3/PP3PP1/6K1 b - - 3 25");
+        Assertions.assertEquals(
+            """
+             8 ....111.
+             7 ..111.11
+             6 ....111.
+             5 ...1.1.1
+             4 .....1..
+             3 .....1..
+             2 ........
+             1 ........
+               abcdefgh""",
+            generateQueenTargets(Side.WHITE, b).toString()
+        );
+    }
+}
