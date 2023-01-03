e213216e0981cd9139f47643f767592ae4eb8f2d
Ted Paulsen <ted.m.paulsen@gmail.com>
2023-01-02 21:35:18 -0700

    Fix indentation

--- src/test/java/io/github/tedpaulsen/chess/lib/movegen/QueenMoveGenTest.java
+++ src/test/java/io/github/tedpaulsen/chess/lib/movegen/QueenMoveGenTest.java
@@ -13,15 +13,15 @@ public class QueenMoveGenTest extends TestBase {
         var b = BoardRepresentation.fromFen("r6k/p1p2Q1p/1bp4p/3pP3/3P2bq/2P1R3/PP3PP1/6K1 b - - 3 25");
         Assertions.assertEquals(
             """
-             8 ....111.
-             7 ..111.11
-             6 ....111.
-             5 ...1.1.1
-             4 .....1..
-             3 .....1..
-             2 ........
-             1 ........
-               abcdefgh""",
+            8 ....111.
+            7 ..111.11
+            6 ....111.
+            5 ...1.1.1
+            4 .....1..
+            3 .....1..
+            2 ........
+            1 ........
+              abcdefgh""",
             generateQueenTargets(Side.WHITE, b).toString()
         );
     }
